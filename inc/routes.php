f8f1ec653e39cf3ee651325c56b7fbbeb4059dfe
Rusty <proga100@gmail.com>
2024-02-29 22:47:46 +0500

    ee

--- inc/routes.php
+++ inc/routes.php
@@ -4,11 +4,18 @@
 require_once 'http/CourseSerializer.php';
 require_once 'GetSettingsController.php';
 require_once 'GetSettings.php';
+require_once 'UpdateSettingsController.php';
 
 $router->get(
 	'/courses/{course_id}/settingschild',
 	\MasterStudy\Lms\Http\Controllers\Course\GetSettingsControllerChild::class,
 	\MasterStudy\Lms\Routing\Swagger\Routes\Course\GetSettingsChild::class
+);
+
+$router->put(
+	'/courses/{course_id}/settingschild',
+	\MasterStudy\Lms\Http\Controllers\Course\UpdateSettingsControllerChild::class,
+	\MasterStudy\Lms\Routing\Swagger\Routes\Course\UpdateSettings::class
 );
 
 
